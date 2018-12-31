5646c98876cd6f2260b3254a6c6bfd06a6bc73bd
Suyash <hackerzden01@gmail.com>
2018-12-31 15:07:23 +0530

    adding files

---
+++ main.js
@@ -0,0 +1,10 @@
+const scroll1=()=>{
+    console.log('click')
+    $('#ba1').css('transform','rotate(180deg)')
+    setInterval(() => {
+        $('#ba1').css('transform', 'rotate(0deg)')
+    }, 900);
+    $('html, body').animate({
+        scrollTop: $(".about-us").offset().top
+    }, 1200);
+}
