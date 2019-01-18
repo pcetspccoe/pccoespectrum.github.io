dfa737ba54d20d6bcd5e172bb17f51f0961e4c43
suyashsonawane <suyashsonawane007@gmail.com>
2019-01-18 22:26:29 +0530

    under construction

--- js/events.js
+++ js/events.js
@@ -1,18 +1,23 @@
 var swiper = new Swiper('.swiper-container', {
-    spaceBetween: 30,
-    hashNavigation: {
+  spaceBetween: 30,
+  hashNavigation: {
       watchState: true,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
+  },
+//   pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//   },
+  navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
-  });
+  },
+});
+swiper.keyboard.enable()
+swiper.on('slideChange', () => {
+  console.log('slide')
+})
 
-  swiper.on('slideChange',()=>{
-      console.log('slide')
-  })
+// swiper for presentation
+
+
+
