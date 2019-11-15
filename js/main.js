d69260e51cd84fbf92a2bbe3a7a15df9f47088e8
Suyash Sonawane <suyashsonawane007@gmail.com>
2019-11-15 16:52:37 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -160,36 +160,36 @@ $(window).scroll(function (event) {
 // });
 // timer
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 31, 2019 :00:00").getTime();
+// var countDownDate = new Date("Mar 31, 2019 :00:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+// // Update the count down every 1 second
+// var x = setInterval(function() {
 
-  // Get todays date and time
-  var now = new Date().getTime();
+//   // Get todays date and time
+//   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   // Time calculations for days, hours, minutes and seconds
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-  document.getElementById("demo-mob").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+//   // Display the result in the element with id="demo"
+//   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+//   + minutes + "m " + seconds + "s ";
+//   document.getElementById("demo-mob").innerHTML = days + "d " + hours + "h "
+//   + minutes + "m " + seconds + "s ";
 
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "31st March, 2019";
-    document.getElementById("demo-mob").innerHTML = "31st March, 2019";
-  }
-}, 1000);
+//   // If the count down is finished, write some text 
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("demo").innerHTML = "31st March, 2019";
+//     document.getElementById("demo-mob").innerHTML = "31st March, 2019";
+//   }
+// }, 1000);
 
 
 const mob_show =() =>{
